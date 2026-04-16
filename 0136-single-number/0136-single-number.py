5e1f4531be4481774c3db45ee6600adcc9ab334a
motohisa1342 <s1320112@u-aizu.ac.jp>
2026-04-16 10:21:38 +0900

    Time: 210 ms (15.49%), Space: 14.1 MB (24.37%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = 0
+        for i in range(len(nums)):
+            xor = xor ^ nums[i]
+            print(xor)
+        return xor
+
+        
