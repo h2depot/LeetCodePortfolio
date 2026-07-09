e552a36a7aff3c17c57a8d8bfc7b98fc7a54ddb7
HalloweenHead・Forge <s1320112@u-aizu.ac.jp>
2026-07-09 14:47:37 +0900

    Time: 9 ms (75.29%), Space: 13 MB (99.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -7,11 +7,14 @@ class Solution(object):
         rows = set()
         columns = set()
 
+        #ここで値が0の要素を集合rows, columnsに纏める
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
                     rows.add(i)
                     columns.add(j)
+        
+        #それぞれの集合の要素を取り出してその行or列上にいる要素も0に書き換える
         for i in rows:
             for j in range(len(matrix[0])):
                 matrix[i][j] = 0
